8deded8abf5ea32b45bc599955263b4d427be054
Olivier Eggimann <oli@olinetz.ch>
2018-11-24 11:08:49 +0100

    merge

--- src/main/java/ch/stair/hackday/packhack/analytics/AnalyticsUtils.java
+++ src/main/java/ch/stair/hackday/packhack/analytics/AnalyticsUtils.java
@@ -4,16 +4,23 @@ import ch.stair.hackday.packhack.agent.Agent;
 import ch.stair.hackday.packhack.dto.Direction;
 import ch.stair.hackday.packhack.dto.FieldTypes;
 
-public class AnalyticsUtils implements Agent{
+import ch.stair.hackday.packhack.dto.Direction;
+import ch.stair.hackday.packhack.dto.FieldTypes;
+
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class AnalyticsUtils implements Agent {
     private static int ENEMY_POINTS = 0;
     private boolean isHunter = false;
-    private FieldTypes[][] lastGameFieldState;
+    private FieldTypes[][] game;
 
     public AnalyticsUtils(FieldTypes[][] initialGameField) {
-        this.lastGameFieldState = initialGameField;
+        this.game = initialGameField;
     }
 
-    public void prepareNextStep(FieldTypes[][] actualGameField, float [] enemyPosition) {
+    public void prepareNextStep(FieldTypes[][] actualGameField, float[] enemyPosition) {
         countEnemyPoints(actualGameField, enemyPosition);
     }
 
@@ -22,11 +29,11 @@ public class AnalyticsUtils implements Agent{
     }
 
 
-    private void countEnemyPoints(FieldTypes[][] actualGameField, float [] enemyPosition) {
-       if (lastGameFieldState[(int) enemyPosition[0]][(int) enemyPosition[1]] == FieldTypes.FOOD) {
-           ENEMY_POINTS++;
-       }
-       lastGameFieldState = actualGameField;
+    private void countEnemyPoints(FieldTypes[][] actualGameField, float[] enemyPosition) {
+        if (game[(int) enemyPosition[0]][(int) enemyPosition[1]] == FieldTypes.FOOD) {
+            ENEMY_POINTS++;
+        }
+        game = actualGameField;
     }
 
     public int getEnemyPoints() {
@@ -41,37 +48,31 @@ public class AnalyticsUtils implements Agent{
     @Override
     public String getAgentInformation() {
         return null;
-    }
-import ch.stair.hackday.packhack.dto.Direction;
-import ch.stair.hackday.packhack.dto.FieldTypes;
 
 
-import java.util.LinkedList;
-import java.util.List;
-
-public class AnalyticsUtils {
-    private List<Direction> getPossibilites(int x, int y){
-        List<Direction> possibilites = new LinkedList<>();
-        if(y-1>=0){ //check north
-            if(game[x][y-1]== FieldTypes.WALL){
-                possibilites.add(Direction.NORTH);
+        private List<Direction> getPossibilites ( int x, int y){
+            List<Direction> possibilites = new LinkedList<>();
+            if (y - 1 >= 0) { //check north
+                if (game[x][y - 1] == FieldTypes.WALL) {
+                    possibilites.add(Direction.NORTH);
+                }
             }
-        }
-        if(x+1<=game.length){ //check east
-            if(game[x+1][y]){
-                possibilites.add(Direction.EAST);
+            if (x + 1 <= game.length) { //check east
+                if (game[x + 1][y]) {
+                    possibilites.add(Direction.EAST);
+                }
             }
-        }
-        if(y+1<=game[0].length){
-            if(game[x][y+1] == FieldTypes.WALL){
-                possibilites.add(Direction.SOUTH);
+            if (y + 1 <= game[0].length) {
+                if (game[x][y + 1] == FieldTypes.WALL) {
+                    possibilites.add(Direction.SOUTH);
+                }
             }
-        }
-        if(x-1>=0){
-            if(game[x-1][y] == FieldTypes.WALL){
-                possibilites.add(Direction.WEST);
+            if (x - 1 >= 0) {
+                if (game[x - 1][y] == FieldTypes.WALL) {
+                    possibilites.add(Direction.WEST);
+                }
             }
+            return possibilites;
         }
-        return possibilites;
     }
 }
