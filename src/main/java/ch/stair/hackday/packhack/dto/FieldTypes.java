bec25df87ecfc2a81f86ddeaea5a6d1c2717c210
CryDeb <CryDeb@users.noreply.github.com>
2018-11-24 08:20:35 +0100

    Update FieldTypes.java

--- src/main/java/ch/stair/hackday/packhack/dto/FieldTypes.java
+++ src/main/java/ch/stair/hackday/packhack/dto/FieldTypes.java
@@ -5,6 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public enum FieldTypes {
     @JsonProperty(" ")EMPTY,
     @JsonProperty("%")WALL,
-    @JsonProperty("°")CAPSULE,
-    @JsonProperty("P")PLAYER
+    @JsonProperty("°")FOOD,
+    @JsonProperty("o")CAPSULE
 }
